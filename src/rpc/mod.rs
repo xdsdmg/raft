6e20983452bcd8c0ac29b8525837b7818ad56c01
xuedishangdemeigui <2216336843@qq.com>
2024-06-29 00:48:08 +0800

    init repo

--- src/rpc/mod.rs
+++ src/rpc/mod.rs
@@ -1,4 +1,7 @@
-use std::net::{TcpListener, TcpStream};
+use std::{
+    io::Read,
+    net::{TcpListener, TcpStream},
+};
 
 pub struct RPC {
     host: String,
@@ -13,5 +16,31 @@ impl RPC {
 
     pub fn spin(&self) {
         let listener = TcpListener::bind(&self.host).expect("error: RPC service startup failed");
+
+        println!("RPC service is running at {}", self.host);
+
+        for stream in listener.incoming() {
+            let stream = match stream {
+                Ok(stream) => stream,
+                Err(e) => {
+                    println!("error: get TCP stream failed, {}", e);
+                    continue;
+                }
+            };
+            self.parse_connection(stream);
+        }
+    }
+
+    fn parse_connection(&self, mut stream: TcpStream) {
+        let mut buffer = [0; 1024];
+
+        if let Err(e) = stream.read(&mut buffer) {
+            println!("error: TCP stream read failed, {}", e);
+        }
+
+        println!(
+            "TCP stream content: {}",
+            std::str::from_utf8(&buffer).unwrap()
+        );
     }
 }
